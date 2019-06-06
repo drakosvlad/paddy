7e2c349566de84bad0a19dccfe713941b56d894d
Vladyslav Bozhko <vladyslav@bozhko.net>
2019-06-06 22:57:05 +0300

    Working JWT auth

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,13 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('auth')->group(function () {
+    Route::post('login', ['uses' => 'AuthController@login']);
+    Route::post('logout', ['uses' => 'AuthController@logout']);
+    Route::post('refresh', ['uses' => 'AuthController@refresh']);
+    Route::post('me', ['uses' => 'AuthController@me']);
+});
+
+Route::get('lol', function () {
+    return "kek";
 });
