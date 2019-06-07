adf22f651d1fdeb0377fd2f9c6ab0a78c596689d
Olha Kirdiaieva <o.kirdiaieva@ukma.edu.ua>
2019-06-07 20:05:05 +0300

    WIP on Login Component

--- resources/js/components/pages/LoginComponent.vue
+++ resources/js/components/pages/LoginComponent.vue
@@ -1,6 +1,15 @@
 <template>
     <div>
-        Login
+        <div class="card col-md-6 col-sm-6 col-12">
+            <span>Welcome!</span>
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username">
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password">
+            <a href="#">Forgot password?</a>
+            <button class="button-secondary">Register</button>
+            <button class="button-primary">Login</button>
+        </div>
     </div>
 </template>
 
